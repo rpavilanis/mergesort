697eabfe6d52fa7aa7f8cdc4905509dbea3edef8
Rachel <rachel.l.pavilanis@gmail.com>
2016-10-06 12:01:32 -0700

    finished methods

--- mergesort.rb
+++ mergesort.rb
@@ -1,25 +1,49 @@
 def mergesort(a)
-    # if the array size is 0|1 then the list is considered sorted, return sorted array
-    # split the list into two halves
-    # merge sort each half
-    # combine the sorted halves
+  # if the array size is 0|1 then the list is considered sorted, return sorted array
+  if a.length <= 1
+    return a
+  end
+  # split the list into two halves
+  left, right = split_array(a)
+  # merge sort each half
+  left = mergesort(left)
+  right = mergesort(right)
+  # combine the sorted halves
+  return combine(left, right)
 end
 
 def split_array(a)
-    # find the middle index
-    # split the array in half - take a look at take and drop in the ruby docs, or use ranges
-    # return left and right halves of array as separate arrays - did you know a method can return multiple arrays? look it up
+  midway = a.length/2
+  return a[0..midway], a[midway+1..-1]
+  # find the middle index
+  # split the array in half - take a look at take and drop in the ruby docs, or use ranges
+  # return left and right halves of array as separate arrays - did you know a method can return multiple arrays? look it up
 end
 
-# precondition: a and b are sorted
+# precondition: a and b are sorted NO recursion in it
 def combine(a, b)
+
     # create a results array
-    # counters pointing to the index of the smallest elements in each array - you may not need to do this step if you use some fancy ruby methods
+    result = []
     # check that we have elements to compare (neither array can be empty)
-        # push the smaller element onto the result array
+    # push the smaller element onto the result array
+    while !a.empty? && !b.empty?
+      if a.first < b.first
+        result << a.shift
+      else
+        result << b.shift
+      end
+    end
     # if there are elements left over in a, move them to result
     # if there are elements left over in b, move them to result
+    while !a.empty?
+      result << a.shift
+    end
+    while !b.empty?
+      result << a.shift
+    end
     # return the results array
+    return result 
 end
 
 # TEST IT
